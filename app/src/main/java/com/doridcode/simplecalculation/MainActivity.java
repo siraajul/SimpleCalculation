3448a1f2456022190da0b5d4bf7efc5f3cf492df
Sirajul <shirajulislamparvez@gmail.com>
2023-02-21 08:28:40 +0600

    Clean Up The Old Code

--- app/src/main/java/com/doridcode/simplecalculation/MainActivity.java
+++ app/src/main/java/com/doridcode/simplecalculation/MainActivity.java
@@ -10,28 +10,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText entername;
-    Button outputbutton;
-    TextView output;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        entername=findViewById(R.id.entername);
-        outputbutton=findViewById(R.id.outputbutton);
-        output=findViewById(R.id.output);
-
-
-        outputbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username = entername.getText().toString();
-                output.setText("Hello "+ username+"\nGreat Work!");
-            }
-        });
-
 
     }
 }
